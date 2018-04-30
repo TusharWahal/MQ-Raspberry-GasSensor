2ad756e049d71c67de01a60517bdc65e7915500e
TusharWahal <tush.wh@gmail.com>
2018-04-30 16:51:29 +0530

    Implemented Post Method

--- src/com/gas/MQApplication.java
+++ src/com/gas/MQApplication.java
@@ -1,6 +1,11 @@
 package com.gas;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Map;
 
 public class MQApplication {
@@ -45,6 +50,34 @@ public class MQApplication {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+	}
+	public static String sendPostRequest(String requestUrl, String payload) {
+
+		StringBuffer jsonString = new StringBuffer();
+		try {
+			URL url = new URL(requestUrl);
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+			connection.setDoInput(true);
+			connection.setDoOutput(true);
+			connection.setRequestMethod("POST");
+			connection.setRequestProperty("Accept", "application/json");
+			connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+			OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
+			writer.write(payload);
+			writer.close();
+			BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+			String line;
+			while ((line = br.readLine()) != null) {
+				jsonString.append(line);
+			}
+			br.close();
+			connection.disconnect();
+		} catch (Exception e) {
+			throw new RuntimeException(e.getMessage());
+		}
+		return jsonString.toString();
 	}
 
 }
